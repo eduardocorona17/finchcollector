cb063cfdf4a5cc467a8fc71f008749023b6dc808
Eduardo Corona <eduardocorona@Eduardos-MacBook-Pro.local>
2022-01-13 19:06:21 -0800

    create basic feed model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
@@ -12,6 +18,17 @@ class Finch(models.Model):
 
   def get_absolute_url(self):
     return reverse('finches_detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+    )
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+  
 
 
 
