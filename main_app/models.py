efd36a797a436b7aac442da65426031d358c8b12
Eduardo Corona <eduardocorona@Eduardos-MacBook-Pro.local>
2022-01-13 19:22:40 -0800

    Feed birdies

--- main_app/models.py
+++ main_app/models.py
@@ -20,12 +20,14 @@ class Finch(models.Model):
     return reverse('finches_detail', kwargs={'finch_id': self.id})
 
 class Feeding(models.Model):
-  date = models.DateField()
+  date = models.DateField('feeding date')
   meal = models.CharField(
     max_length=1,
     choices=MEALS,
     default=MEALS[0][0]
     )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+  
   def __str__(self):
     return f"{self.get_meal_display()} on {self.date}"
   
