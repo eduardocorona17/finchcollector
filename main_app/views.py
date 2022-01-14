1078d5859166c17d19d72c293f7f9d3260f494ac
Eduardo Corona <eduardocorona@Eduardos-MacBook-Pro.local>
2022-01-13 18:31:51 -0800

    second part finish

---
+++ main_app/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Finch
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def finches_index(request):
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', {'finches': finches})
+
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
+
+
+# class FinchCreate(CreateView):
+#     model = Finch
+#     fields = '__all__'
+#     success_url = '/finches/'
+
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['name', 'description', 'age']
+
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
+
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = ['name', 'description', 'age']
