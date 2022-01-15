3f541e359a4a2785678bde9fe90f038abcaa7383
Eduardo Corona <eduardocorona@Eduardos-MacBook-Pro.local>
2022-01-14 19:51:51 -0800

    Final CRUD

--- main_app/models.py
+++ main_app/models.py
@@ -8,10 +8,22 @@ MEALS = (
     ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+  
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id}) 
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
+  
 
   def __str__(self):
      return self.name
@@ -31,7 +43,7 @@ class Feeding(models.Model):
   def __str__(self):
     return f"{self.get_meal_display()} on {self.date}"
     class Meta:
-      ordering = ['-date']
+     ordering = ['-date']
   
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
 
 
@@ -21,15 +21,47 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     feeding_form = FeedingForm()
-    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form})
+    toys_finch_doesnt_have = Toy.objects.exclude(
+        id__in=finch.toys.all().values_list('id'))
+    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have})
+
 
 def add_feeding(request, finch_id):
-  form = FeedingForm(request.POST)
-  if form.is_valid():
-    new_feeding = form.save(commit=False)
-    new_feeding.finch_id = finch_id
-    new_feeding.save()
-  return redirect('detail', finch_id=finch_id)
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('finches_detail', finch_id=finch_id)
+
+
+class ToyList(ListView):
+    model = Toy
+
+
+class ToyDetail(DetailView):
+    model = Toy
+
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+
+def assoc_toy(request, finch_id, toy_id):
+    finch = Finch.objects.get(id=finch_id)
+    finch.toys.add(finch_id)
+    return redirect('finches_detail', finch_id=finch_id)
 
 
 class FinchCreate(CreateView):
